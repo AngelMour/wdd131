71d54ba9a74f2d7162141c46feadd29d11c1ecee
AngelMour <angelmoure79@gmail.com>
2025-03-09 20:51:50 -0500

    Week 1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set the current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display last modified date
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
